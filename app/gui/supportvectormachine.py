de741e526b9760d5fdac884d46ac942ff76abf33
ByteWaiser <bytewaiser@gmail.com>
2021-03-19 13:29:04 +0000

    Fix for SVM again

--- app/gui/supportvectormachine.py
+++ app/gui/supportvectormachine.py
@@ -515,14 +515,15 @@ class SupportVectorMachine:
                 raise Exception
 
             for i, j in enumerate(["Epsilon", "Nu", "C", "Gamma", "Coef0", "Degree"]):
-                if self.model_parameters_frame_options[i][1]["state"] != tk.DISABLED and not self.parameters[i].get():
+                if self.model_parameters_frame_options[i][1]["state"] != "disabled" and not self.parameters[i].get():
                     msg = "Enter a valid " + j +  " value"
                     raise Exception
                 
                 if self.grid_option_var.get():
-                    if (self.model_parameters_frame_options[i][2]["state"] != tk.DISABLED):
-                        msg = "Enter a valid " + j +  " value in grid search area"
-                        raise Exception
+                    if self.model_parameters_frame_options[i][2]["state"] != "disabled":
+                        if (not self.optimization_parameters[i][0].get() or not self.optimization_parameters[i][1].get()):
+                            msg = "Enter a valid " + j +  " value in grid search area"
+                            raise Exception
 
         except:
             popupmsg(msg)
