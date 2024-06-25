f2847784a3dbe33da23534078244f1053e422f22
Bytewaiser <bytewaiser@gmail.com>
2024-06-25 09:52:39 +0300

    backend/lookback.py started

--- app/gui/backend/lookback.py
+++ app/gui/backend/lookback.py
@@ -1,29 +1,61 @@
 import numpy as np
-from utils import shift_array, concat_X_y
-
-
+from utils import shift_array
 
 
 class Lookback:
-    def __init__(self):
+    def __init__(
+        self,
+        lookback_count: int,
+        seasonal_lookback_count: int,
+        seasonal_lookback_period: int,
+    ):
+        xnor = lambda x, y: (x and y) or (not x and not y)
 
-        pass
+        assert xnor(
+            seasonal_lookback_count > 0, seasonal_lookback_period > 0
+        ), "Error"
 
-    def get_lookback(self, X, y, lookback):
-        pass
+        self.lookback_count = lookback_count
+        self.seasonal_lookback_count = seasonal_lookback_count
+        self.seasonal_lookback_period = seasonal_lookback_period
+
+        self.lookback_index = list(range(1, lookback_count + 1, 1))
+        self.seasonal_lookback_index = list(
+            range(
+                seasonal_lookback_period,
+                (seasonal_lookback_count + 1) * seasonal_lookback_period,
+                max(seasonal_lookback_period, 1),
+            )
+        )
+
+        self.merged_index = list(
+            set(self.lookback_index + self.seasonal_lookback_index)
+        )
+
+    def __get_lookback_stack(self, y: np.ndarray, n):
+        stack = y.copy().reshape(-1, 1)
+        for i in range(1, n + 1):
+            stack = np.hstack((stack, shift_array(y, i).reshape(-1, 1)))
+        return stack
+
+    def get_lookback(self, X: np.ndarray, y: np.ndarray):
+        if self.merged_index:
+            stack = self.__get_lookback_stack(y, max(self.merged_index))
+            stack = stack[:, self.merged_index]
+            X = np.concatenate((X, stack), axis=1)
+
+            X = X[~np.isnan(X).any(axis=1)]
+            y = y[y.shape[0] - X.shape[0] :]
+
+        return X, y
 
 
+X = np.random.rand(15, 4).round(2)
+y = np.arange(15)
 
 
-
-
-X = np.random.rand(10, 4).round(2)
-y = np.arange(10)
-
-
-for i in range(1, 4):
-    X = np.concatenate((X, shift_array(y, i).reshape(-1, 1)), axis=1)
-
+lookback = Lookback(9, 6, 2)
+X, y = lookback.get_lookback(X, y)
 print(X)
 print(y)
 
--- app/gui/backend/utils.py
+++ app/gui/backend/utils.py
@@ -1,27 +1,25 @@
-import tkinter as tk
-
-
 import pickle
-import numpy as np
+import tkinter as tk
 from typing import Any
 
-def shift_array(array: np.ndarray, n: int):
+import numpy as np
+
+def shift_array(array: np.ndarray, n: int) -> np.ndarray:
     assert array.ndim == 1, "array must be a 1D array"
     assert n >= 0, "n must be a non-negative integer"
+
+    if n == 0:
+        return array
+
     return np.concatenate((np.full(n, np.nan), array[:-n]))
 
-def concat_X_y(X: np.ndarray, y: np.ndarray):
-    assert X.ndim == 2, "X must be a 2D array"
-    assert y.ndim == 1, "y must be a 1D array"
-    assert X.shape[0] == y.shape[0], "X and y must have the same number of rows"
 
-    return np.concatenate((X, y.reshape(-1, 1)), axis=1)
-
-def pickle_dump(obj: Any, file: str):
+def pickle_dump(obj: Any, file: str) -> None:
     with open(file, "wb") as f:
         pickle.dump(obj, f)
 
-def pickle_load(file: str):
+
+def pickle_load(file: str) -> None:
     with open(file, "rb") as f:
         return pickle.load(f)
 
@@ -33,7 +31,7 @@ def cartesian(*arrays):
     flat = np.concatenate(mesh).ravel()
     return np.reshape(flat, (dim, elements)).T
 
+
 def popupmsg(msg):
     tk.messagebox.showinfo("!", msg)
     return False
-
